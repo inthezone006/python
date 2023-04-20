1510612dccc71003665e08f189c50978c9751fc9
Rahul Menon <rahul.menon85280@gmail.com>
2023-04-19 22:25:05 -0400

    all

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
-class Person:
-    def __init__(self, fname, lname):
-        self.fname = fname
-        self.lname = lname
+import platform as pf
+import people
+import car
 
-    def printName(self):
-        return self.fname, self.lname
+#Module and Class Example #1
+def test1():
+    print(people.Person("John", "Smith").printInfo())
+    print(people.Student("Boring", "Bobby", 2022).printInfo())
+    print(people.Teacher("Lame", "Larry", 10).printInfo())
+    print()
 
-x = Person("John", "Smith")
-print(x.printName()[0], x.printName()[1])
+#Module and Class Example #2
+def test2():
+    print(car.Car("Lamborghini", "Aventador").returnCar())
+    print(car.BMW("M550I").returnCar())
+    print(car.Toyota("86").returnCar())
+    print()
+
+#Another Module Example
+def test3():
+    print("Platform Architecture:", pf.machine())
+    print()
