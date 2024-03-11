79f517f7400fc81ed33aaa2e8ea1fd096428beea
Rahul Menon <rahul.menon85280@gmail.com>
2024-03-11 10:57:54 -0500

    All

--- flask_example/main.py
+++ flask_example/main.py
@@ -1,2 +1,10 @@
-from flask import flask
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+if __name__=="__main__":
+    app.run(debug=True)
